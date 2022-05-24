a6e238ecc1d91be1cebf3c83a808e41f1e817b45
tenzin1308 <tenztashi8@gmail.com>
2022-05-24 19:21:21 -0400

    feat: about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AboutSection from '../components/AboutSection';
 import HeroSection from '../components/HeroSection';
+import SocialMediaSection from '../components/SocialMediaSection';
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
+      <SocialMediaSection />
     </div>
   );
 }
