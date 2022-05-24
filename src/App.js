543d6b5fc95a2bdc9f9145f2ca7b2df7e428ff21
tenzin1308 <tenztashi8@gmail.com>
2022-05-24 18:45:12 -0400

    code cleanup: portfolio v.2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import About from "./components/About";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-// import Post from "./components/Post";
-import Project from "./components/Project";
-import SinglePost from "./components/SinglePost";
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route component={Home} path="/" exact />
-        <Route component={About} path="/about" />
-        <Route component={SinglePost} path="/post/:slug" />
-        <Route component={Project} path="/Project" />
-        {/* <Route component={Project} path='/project' /> */}
-      </Switch>
+      <Switch></Switch>
     </BrowserRouter>
   );
 }
