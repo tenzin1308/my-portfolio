5a3b6925c11868dd5c65c62b30908a5cddbe361e
tenzin1308 <tenztashi8@gmail.com>
2022-05-24 22:57:54 -0400

    feat: contact page completed

---
+++ src/components/ContactForm.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FormStytle = styled.form`
+  width: 100%;
+  .form-group {
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  label {
+    font-size: 1.8rem;
+  }
+  input,
+  textarea {
+    width: 100%;
+    padding: 1.2rem;
+    font-size: 2rem;
+    color: var(--gray-1);
+    background-color: var(--deep-dark);
+    border: none;
+    outline: none;
+    border-radius: 8px;
+    margin-top: 1rem;
+  }
+  textarea {
+    min-height: 200px;
+    resize: vertical;
+  }
+  button[type='submit'] {
+    background-color: var(--gray-1);
+    color: var(--black);
+    font-size: 2rem;
+    display: inline-block;
+    outline: none;
+    border: none;
+    padding: 1rem 4rem;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
+export default function ContactForm() {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  return (
+    <div>
+      <FormStytle>
+        <div className="form-group">
+          <label htmlFor="name">
+            Your name
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">
+            Your email
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">
+            Your message
+            <textarea
+              type="text"
+              id="message"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Send</button>
+      </FormStytle>
+    </div>
+  );
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import ContactSection from '../components/ContactSection';
+import SocialMediaSection from '../components/SocialMediaSection';
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <ContactSection />
+      <SocialMediaSection />
+    </div>
+  );
 }
