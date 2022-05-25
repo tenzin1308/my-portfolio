c2a2d9e62ad8eb86cf3d53315cdf0c6ec0e257e2
tenzin1308 <tenztashi8@gmail.com>
2022-05-24 22:48:34 -0400

    feat: about section completed

---
+++ src/components/AboutInfoItem.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import PText from './PText';
+
+const AboutInfoItemStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  margin-top: 3rem;
+  .title {
+    font-size: 2.4rem;
+  }
+  .items {
+    display: flex;
+    gap: 1.5rem;
+    position: absolute;
+    left: 35rem;
+    ${'' /* left: 18rem; */}
+  }
+  .item {
+    background-color: var(--deep-dark);
+    padding: 1rem;
+    border-radius: 8px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    .items {
+      position: initial;
+      gap: 1rem;
+    }
+    .title {
+      font-size: 2rem;
+    }
+  }
+`;
+
+export default function AboutInfoItem({
+  title = 'title',
+  items = ['C++', 'JAVA', 'REACT'],
+}) {
+  return (
+    <AboutInfoItemStyle>
+      <h1 className="title">{title}</h1>
+      <div className="items">
+        {items.map((item, index) => (
+          <div className="item" key={index}>
+            <PText className="item-text">{item}</PText>
+          </div>
+        ))}
+      </div>
+    </AboutInfoItemStyle>
+  );
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,163 @@
 import React from 'react';
+import styled from 'styled-components';
+import AboutImg from '../assets/images/about-page-img.jpg';
+import AboutInfoItem from '../components/AboutInfoItem';
+import Button from '../components/Button';
+import ContactBanner from '../components/ContactBanner';
+import PText from '../components/PText';
 
+const AboutPageStyle = styled.div`
+  padding: 20rem 0 10rem 0;
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--deep-dark);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--gray-1);
+    }
+  }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-top: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
+`;
 export default function About() {
-  return <div>About</div>;
+  return (
+    <AboutPageStyle>
+      <div className="container">
+        <div className="top-section">
+          <div className="left">
+            <p className="about__subheading">
+              Hi i am <span>Tenzin Tashi</span>
+            </p>
+            <h2 className="about__heading">Software Engineer</h2>
+            <div className="about__info">
+              <PText>
+                I am currently a senior at City College of New York getting my
+                bachelor’s degree in computer science, my interests lie in the
+                field of programming. Hence, during my journey I have worked on
+                many projects in additional to the class project. Also, last
+                summer I did 6 months long internship as software engineer in a
+                financial company where I gained lots of hands-on experience and
+                get a chance to put my knowledge to practice.
+                <br /> <br />
+                Software engineering is a field where there’s always room to
+                learn and grow, and I am a person who’s always had an eager to
+                learn more and learn new things. What I also find exciting about
+                the field is the proactivity that it requires, the fast pace of
+                change and the problem solving. With the wave of technology
+                there are always new thing coming out and I find that thrilling.
+                <br /> <br />
+                My career goal is to start as a software engineer, later with
+                the experience and knowledge I gain I’d become a senior/staff
+                engineer.
+                <br /> <br />
+              </PText>
+            </div>
+            {/* Link the resume here */}
+            <Button btnText="Download CV" btnLink="#" />
+          </div>
+          <div className="right">
+            <img src={AboutImg} alt="Tenzin Tashi" />
+          </div>
+        </div>
+        <div className="about__info__items">
+          {/* Education */}
+          <div className="about__info__item">
+            <h1 className="about__info__heading">Education</h1>
+            <AboutInfoItem
+              title="School"
+              items={['City College of New York (CCNY)']}
+            />
+            <AboutInfoItem title="Major" items={['Computer Science']} />
+            <AboutInfoItem title="GPA" items={['3.844']} />
+            <AboutInfoItem title="Graduation Year" items={['June 2022']} />
+          </div>
+          {/* Experience */}
+          <div className="about__info__item">
+            <h1 className="about__info__heading">Experience</h1>
+            <AboutInfoItem
+              title="Software Engineer"
+              items={['American Express', 'Aug 2022 - Present']}
+            />
+            <AboutInfoItem
+              title="Software Engineer (Intern)"
+              items={['Numeraxial LLC', 'Jun 2021 - Dec 2021']}
+            />
+            <AboutInfoItem
+              title="Student Technology Mentor"
+              items={['LaGuardia Community College', 'July 2019 - Jun 2022']}
+            />
+            <AboutInfoItem
+              title="Academic Peer Instructor"
+              items={['LaGuardia Community College', 'March 2019 - Jun 2019']}
+            />
+          </div>
+          {/* Skill */}
+          <div className="about__info__item">
+            <h1 className="about__info__heading">My Skill</h1>
+            <AboutInfoItem
+              title="Programming Languages"
+              items={['Java', 'Python', 'C++', 'React']}
+            />
+            <AboutInfoItem
+              title="Tools"
+              items={['NetBeans', 'XCode', 'VSCode', 'PyCharm']}
+            />
+          </div>
+        </div>
+      </div>
+      <ContactBanner />
+    </AboutPageStyle>
+  );
 }
